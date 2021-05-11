755df5d2976c7f935ab7ad24225c4eddcba57c10
akshay1120 <asjain3031@gmail.com>
2021-05-11 09:40:48 +0530

    Adding AutoIT

--- ApachePOI_OJT/src/test/java/autoIt/Demo.java
+++ ApachePOI_OJT/src/test/java/autoIt/Demo.java
@@ -1,5 +1,19 @@
 package autoIt;
 
-public class Demo {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Demo 
+{
+	public void AutoIt () throws Exception
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("file:///E:/AutoIT/Upload.html");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("input")).click();
+		Thread.sleep(3000);
+		Runtime.getRuntime().exec("E:\\AutoIT\\demo.exe");
+	}
 }
